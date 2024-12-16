3f8b5869f7309db35f42f62f2e7eadf9551d5e20
Dilan Pramodhya <dilanpramodhya44@gmail.com>
2024-12-16 23:37:58 +0530

    Create, GetAll, Delete & Update Products controller, models and routes done in backend

---
+++ backend/routes/product.route.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  GetProducts,
+  CreateProduct,
+  DeleteProduct,
+  UpdateProduct,
+} from "../controller/product/controller.js";
+
+const router = express.Router();
+
+router.get("/", GetProducts);
+router.post("/", CreateProduct);
+router.put("/:id", UpdateProduct);
+router.delete("/:id", DeleteProduct);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
 import dotenv from "dotenv";
 import express from "express";
 import { ConnectDB } from "./config/db.js";
+import router from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-});
+app.use(express.json());
 
 console.log(process.env.MONGO_URI);
+
+app.use("/api/product", router);
 
 app.listen(5000, () => {
   ConnectDB();
