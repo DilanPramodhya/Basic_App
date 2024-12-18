aa100f4c8cde1bae2edfe0b5a406a25ea20f782a
Dilan Pramodhya <dilanpramodhya44@gmail.com>
2024-12-18 19:42:44 +0530

    Foundation for deployment done

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import { ConnectDB } from "./config/db.js";
 import router from "./routes/product.route.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
@@ -11,11 +12,20 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 
 console.log(process.env.MONGO_URI);
 
 app.use("/api/product", router);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
   ConnectDB();
